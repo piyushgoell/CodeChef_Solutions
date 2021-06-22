aa5ce3bdb11ea2143c8dc6003c649b130acc39e1
Piyush Goel <piyushgoel.in@gmail.com>
2021-06-22 22:27:42 +0530

    updateed

--- June Cook Off/TooMuchXor.java
+++ June Cook Off/TooMuchXor.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class TooMuchXor {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++)
+                arr[i] = sc.nextInt();
+            if(n == 1)
+                System.out.println("0");
+            else if(n == 2)
+                System.out.println( (arr[0]^arr[1]) > 0 ? "0" : "-1");
+            else if(n == 3){
+                
+            }
+        }
+
+        sc.close();
     }
 }
  
